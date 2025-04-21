a6539990f64215070fd2cd1696b3ae406f249255
Nayem1004 <nayem@procoder.org>
2025-04-21 12:30:36 +0600

    function practice

---
+++ index.js
@@ -0,0 +1,37 @@
+function add(a, b){
+    let result = a+b;
+    console.log(result)
+}
+// add(10, 20)
+
+function sub(x, y){
+    let result = x - y;
+    console.log(result);
+}
+//  sub(20,10)
+
+arr = [1, 2, 3, 4, 5]
+function sumOfArray(arr){
+    let sum = 0;
+    for(var i = 0; i < arr.length; i++){
+        sum += arr[i]
+    }
+    console.log(sum)
+}
+
+// sumOfArray(arr)
+
+function names(){
+    for (var i = 0; i < arguments.length; i++)
+        console.log(arguments[i])
+}
+// names("nayemshake")
+
+function person(name, email){
+    return{
+        name: name,
+        email: email
+    }
+}
+var p = person('Nayem', 'nayem@procoder.org')
+console.log(p)
---
+++ test.js
@@ -0,0 +1,9 @@
+function person(name, email){
+    return{
+        name: name,
+        email:  email 
+        
+    }
+}
+var p = person('Nayem', 'nayem@procoder.org')
+console.log(p)
